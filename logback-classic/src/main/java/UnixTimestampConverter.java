4e972f81cdcc94630428a9e6a13fbfb9b3a20d73
Luca Zenobi <luzenobi@gmail.com>
2019-12-19 00:53:47 +0100

    Resource inclusion and conversion rule definition

---
+++ logback-classic/src/main/java/UnixTimestampConverter.java
@@ -0,0 +1,11 @@
+import ch.qos.logback.classic.pattern.ClassicConverter;
+import ch.qos.logback.classic.spi.ILoggingEvent;
+
+public class UnixTimestampConverter extends ClassicConverter {
+
+  @Override
+  public String convert(ILoggingEvent event) {
+    return Long.toString(event.getTimeStamp());
+  }
+
+}
